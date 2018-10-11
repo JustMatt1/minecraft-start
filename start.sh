a93d035276281a520ceaa6e8ad045122d212d6cc
Matthew Hogan <matt@matthogan.co.uk>
2018-10-10 23:15:49 -0700

    Update script

--- start.sh
+++ start.sh
@@ -1,23 +1,42 @@
 #!/usr/bin/env bash
+# Written by Matthew Hogan <matt@matthogan.co.uk>, June 2018
+
+######### Configuration #########
+
+NAME="helloworld"
+JAR="spigot.jar"
+RAM=2
+COUNTDOWN=5
+
+#################################
+
+# Accept EULA.
+# Manually beacuse "-Dcom.mojang.eula.agree=true" was added for only builds >Spigot#1544
+echo "eula=true" > ./eula.txt
 
 while true
 do
-        # Change this to whatever.
-        java -d64 -Xmx12G -Xms12G -Dfile.encoding=UTF-8 -jar spigot*.jar
+        java -Dmc=${NAME} \
+            -d64 \
+            -XX:+UseG1GC \
+            -Xmx${RAM}G \
+            -Xms${RAM}G \
+            -Dfile.encoding=UTF-8 \
+            -DIReallyKnowWhatIAmDoingISwear \
+            -jar $JAR \
+            --log-strip-color \
 
-        echo -e "\n\n[Reboot] Starting automated reboot process"
+        echo -e "\n\n[Reboot] Starting automated reboot process. CTRL+C to cancel"
         printf "[Reboot] "
 
-        for i in 3 2 1
+        # Countdown for the restart, so that you have time to CTRL+C
+        for ((i = $COUNTDOWN; i != 0; i--))
         do
-
                 printf "$i..."
                 sleep 1
-
         done
 
         echo -e "\n[Reboot] Clearing console and running Java process"
         sleep 1
-	clear
-
+    clear
 done
